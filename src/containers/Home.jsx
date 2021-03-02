29a76b3f5912c93ce6c0b1bc0eaac74472a5c155
Jair Neri <yayodcv@gmail.com>
2021-03-02 13:39:45 -0600

    Layout v-0.1 ready

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from '@reach/router';
 import Layout from './Layout';
+import ArrowDesck from '../assets/react/ArrowDesck';
 import '../styles/containers/Home.scss';
 const Home = () => (
   <Layout>
@@ -17,7 +18,9 @@ const Home = () => (
             <small>Lose weught</small>
             <small>8 weekends </small>
           </p>
-          <button>Watch Now</button>
+          <button>
+            Watch Now <ArrowDesck />
+          </button>
         </div>
         <div className="slider">
           <figure className="slider__img">
@@ -25,8 +28,25 @@ const Home = () => (
           </figure>
         </div>
       </div>
-      <div className="Layout__categories">Categorias || Filtros </div>
-      <div className="Layout__populars">Populares</div>
+      <div className="Categories">
+        <h2 className="Categories__title">What do you want to train today</h2>
+        <div className="Categories__List">
+          <div className="Categories__filter"></div>
+          <div className="Categories__filter"></div>
+          <div className="Categories__filter"></div>
+          <div className="Categories__filter"></div>
+        </div>
+      </div>
+      <div className="Populars">
+        <h3 className="Populars__title">Lose weight</h3>
+        <div className="Populars__List">
+          <div className="Populars__card --span2 --principal"></div>
+          <div className="Populars__card --principal"></div>
+          <div className="Populars__card "></div>
+          <div className="Populars__card "></div>
+          <div className="Populars__card "></div>
+        </div>
+      </div>
     </div>
   </Layout>
 );
